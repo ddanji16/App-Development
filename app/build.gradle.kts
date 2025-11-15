cac677b63acdf882ad33089999b5a2bc98053154
danielanthonydelacruz <anthonydelac1602@gmail.com>
2025-11-15 09:20:10 +0800

    error files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,28 +3,16 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapp"
-    compileSdk = 36
+    namespace = "com.example.yourapp"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.myapp"
-        minSdk = 24
-        targetSdk = 36
+        minSdk = 23
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -32,11 +20,11 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
